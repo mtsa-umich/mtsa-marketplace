0051b86ce325154ac2455e8921bff23a584695be
yi-cheng-liu <liuyiche@umich.edu>
2023-10-10 21:15:52 -0400

    add login function with umich.edu

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,23 +8,11 @@ const Logo = () => {
   const router = useRouter();
 
   return (
-    // <a
-    //   href='/'
-    //   className='flex flex-row items-center'>
-    //   <Image
-    //     alt='logo'
-    //     src= '/images/michigan.png'
-    //     className='hidden md:block cursor-pointer'
-    //     height='70'
-    //     width='70'
-    //   />
-    //   <h1 className='ml-2 text-3xl text-[#00274C] font-extrabold cursor-pointer'>MTSA Marketplace</h1>
-    // </a>
     <a href='/'>
       <Image
           alt='logo'
           src= '/images/marketplace-color-logo.png'
-          className='block cursor-pointer p-3'
+          className='block cursor-pointer pb-3 px-4 pt-4'
           height='65'
           width='260'
         />
